d0f7ac06b6e0a810c8f86d8b68e4132b71f58665
RV8V <rv.volovik88@gmail.com>
2022-03-08 22:22:20 +0200

    chore: update eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,21 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["prettier", "airbnb-base"],
-  plugins: ["prettier"],
+  extends: ['prettier', 'airbnb-base'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
   rules: {
-    "prettier/prettier": "error"
-  }
-}
+    'prettier/prettier': 'error',
+    'implicit-arrow-linebreak': ['warn', 'beside'],
+    indent: ['warn'],
+    'operator-linebreak': ['warn', 'after'],
+  },
+};
