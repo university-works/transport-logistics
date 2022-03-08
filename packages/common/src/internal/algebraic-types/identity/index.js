08229ddbb6b7cefc77a00c49e2403e2d699c9b42
RV8V <rv.volovik88@gmail.com>
2022-03-08 22:24:36 +0200

    feat: add identity functors helpers

---
+++ packages/common/src/internal/algebraic-types/identity/index.js
@@ -0,0 +1,18 @@
+/**
+ * @linear flow with container style types
+ * @capture each assignment in minimal context(each fn in map)
+ * @keep mapping and folding, composing different ways around id
+ *
+ * @nested functors: .fold.map === .map.fold
+ */
+
+/**
+ * @monadic interface
+ *
+ * @of and @chain(flatMap, bind, >>=, pure) creates monadic interface
+ * @key point monads allow us to nest computation
+ */
+
+const identity = require('./linear-flow-with-container-style-types');
+
+module.exports = identity;
