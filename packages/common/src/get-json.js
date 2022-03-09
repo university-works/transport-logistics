df207e3a2d4809f6bd17d27a58eee383d8599a75
RV8V <rv.volovik88@gmail.com>
2022-03-09 21:26:03 +0200

    feat: safe get json from http client

---
+++ packages/common/src/get-json.js
@@ -0,0 +1,13 @@
+const { curry } = require('ramda');
+
+const httpGet = require('./http-get');
+const safeParse = require('./safe-parse');
+
+const eitherToFuture = require('./internal/natural-transformations/convertation-between-algebraic-types/either-to-future');
+
+/** @: getJson :: HttpClient -> Url -> Future Error JSON  */
+const getJson = curry((client, url) =>
+  httpGet(client)(url).map(safeParse).chain(eitherToFuture),
+); //eslint-disable-line
+
+module.exports = getJson;
