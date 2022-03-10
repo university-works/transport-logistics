c22b85fb7066cca3a6ec26fd8c68a1fc3ecb83d0
RV8V <rv.volovik88@gmail.com>
2022-03-10 21:43:46 +0200

    fix: make average function safe using either

--- packages/common/src/avg.js
+++ packages/common/src/avg.js
@@ -1,6 +1,13 @@
-const { converge, divide, sum, length } = require('ramda');
+const { converge, divide, sum, length, compose } = require('ramda');
+
+const {
+  toEither,
+} = require('./internal/natural-transformations/cast-scalar-to-type/to-ramda-either');
 
 /** @: avg :: List -> Average Value in List */
-const avg = converge(divide, [sum, length]);
+const avg = compose(
+  toEither('value in list are not integer or float types'),
+  converge(divide, [sum, length]),
+);
 
 module.exports = avg;
