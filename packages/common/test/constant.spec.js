a306c50b829923e13d87d300e1b09010a4f58894
RV8V <rv.volovik88@gmail.com>
2022-03-11 12:22:41 +0200

    test: add test for constant function

---
+++ packages/common/test/constant.spec.js
@@ -0,0 +1,25 @@
+const assert = require('assert').strict;
+const chainWithId = require('./utils/chain-with-id');
+const wrapWithEither = require('./utils/wrap-with-either');
+
+const constant = require('../src/constant');
+
+describe('constant fn to return passed value and any value types', () => {
+  const message = 'passed value in null or undefined';
+  const eitherConstant = wrapWithEither(message, constant);
+
+  describe('success way from constant function with either right inside', () => {
+    const selectValueFromEither = chainWithId(eitherConstant);
+
+    it('should return constant means same value', () => {
+      const integer = 9;
+      assert.strictEqual(integer, selectValueFromEither(integer));
+    });
+  });
+
+  describe('failed way from constant function with either left inside', () => {
+    it('should return eitherConstant message because of nullable value type', () => {
+      assert.strictEqual(message, eitherConstant(null).value);
+    });
+  });
+});
