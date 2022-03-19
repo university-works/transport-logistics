c846746047422c0d1f307ada8d360be2fadab136
RV8V <rv.volovik88@gmail.com>
2022-03-19 16:50:24 +0200

    feat: set up jobs and server

--- packages/scheduler/server.js
+++ packages/scheduler/server.js
@@ -1,39 +1,27 @@
 const http = require('http');
 
-// import { setupMq } from './services/amqp/index.js';
-// import { setupJobs } from './services/jobs.js';
-
-// import healthCheck from './utils/healthCheck.js';
-// import { logger } from './utils/logger.js';
+const setupMq = require('./services/amqp/index');
+const setupJobs = require('./services/jobs');
 
 const config = require('./config/config.service');
 
-const healthCheck = require('./utils/health-check');
-const logger = require('./utils/logger');
-
-// import { PORT } from './config.js';
+const { healthCheck, logger } = require('./utils/index');
 
 const server = http.createServer(healthCheck);
 
-server.on(
-  'listening',
-  () => logger.info(`Server listening on port ${config.get('PORT')}`),
-  //   console.log(`Server listening on port ${config.get('PORT')}`),
+server.on('listening', () =>
+  logger.info(`Server listening on port ${config.get('PORT')}`),
 );
 
 Promise.resolve()
-  //   .then(setupMq)
-  //   .then(setupJobs)
+  .then(setupMq)
+  .then(setupJobs)
   .then(() => server.listen(config.get('PORT')));
 
 process.on('unhandledRejection', (error) => {
-  console.log({ error });
-  //   logger.error(error, 'Unhandled rejection');
   throw error;
 });
 
 process.on('uncaughtException', (error) => {
-  console.log({ error });
-  //   logger.error(error, 'Uncaught exception');
   throw error;
 });
