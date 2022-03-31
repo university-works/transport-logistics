54b19f6ea5a5b87f3e244237db25e8a081b2b764
RV8V <rv.volovik88@gmail.com>
2022-04-01 00:43:59 +0300

    feat: set up infa and dev env

---
+++ packages/server/knexfile.js
@@ -0,0 +1,23 @@
+const { config } = require('./config/index');
+
+console.log({ mode: config.get('NODE_ENV') });
+
+module.exports = {
+  [config.get('NODE_ENV')]: {
+    client: config.get('RDBMS_CLIENT'),
+    connection: {
+      port: config.get('POSTGRES_PORT'),
+      host: config.get('HOST'),
+      database: config.get('POSTGRES_DB'),
+      user: config.get('POSTGRES_USER'),
+      password: config.get('POSTGRES_PASSWORD'),
+    },
+    migrations: {
+      directory: config.get('POSTGRES_MIGRATIONS_DIR'),
+    },
+    seeds: {
+      directory: config.get('POSTGRES_SEEDS_DIR'),
+    },
+    debug: config.get('POSTGRES_DEBUG'),
+  },
+};
