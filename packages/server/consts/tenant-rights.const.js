b772d50fdc7b9c157e806fe07569d3d56192396a
RV8V <rv.volovik88@gmail.com>
2022-04-01 16:57:19 +0300

    chore: update constansts for server package

---
+++ packages/server/consts/tenant-rights.const.js
@@ -0,0 +1,10 @@
+const TENANT_RIGHT = {
+  view: 'view',
+  edit: 'edit',
+  delete: 'delete',
+  read: 'read',
+};
+
+const TENANT_RIGHTS = Object.values(TENANT_RIGHT);
+
+module.exports = { TENANT_RIGHT, TENANT_RIGHTS };
