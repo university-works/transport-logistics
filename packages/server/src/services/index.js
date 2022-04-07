818e78eaf1b1798b41f47ddd8d651d22ee4ca3eb
RV8V <rv.volovik88@gmail.com>
2022-04-07 20:57:40 +0300

    feat: add construct repository and base route fns

--- packages/server/src/services/index.js
+++ packages/server/src/services/index.js
@@ -2,6 +2,9 @@ const { requireModules } = require('../../utils/index');
 
 const fns = {
   'set-up-routes.service': 'setUpRoutes',
+  'cache-repo.service': 'cacheRepo',
+  'construct-base-repository.service': 'baseRepository',
+  'construct-universal-route.service': 'universalRoute',
 };
 
 module.exports = requireModules(__dirname)(fns);
