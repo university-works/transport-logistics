2a75b5e74d77fd57217ee56d6417a35313fdcfcc
RV8V <rv.volovik88@gmail.com>
2022-04-08 17:42:15 +0300

    feat: add basic crud generator and views

--- packages/server/src/repos/index.js
+++ packages/server/src/repos/index.js
@@ -2,6 +2,7 @@ const requireModules = require('../../utils/require-modules.utils');
 
 const fns = {
   'state.repository': 'stateRepository',
+  'user.repository': 'userRepository',
 };
 
 module.exports = requireModules(__dirname)(fns);
