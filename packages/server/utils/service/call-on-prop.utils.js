65c1ba8fd277f5c613fbbf538d6e029480d107b8
RV8V <rv.volovik88@gmail.com>
2022-04-11 12:09:30 +0300

    chore: update modules and services

--- packages/server/utils/service/call-on-prop.utils.js
+++ packages/server/utils/service/call-on-prop.utils.js
@@ -1,29 +1,9 @@
-const { curry, compose, always, call, prop } = require('ramda');
-
-// /** @: callOnProp :: prop -> obj -> call obj[prop] */
-// const callOnProp = curry(compose(always, call, prop));
-
-// /** @: callOnProp :: prop -> obj -> call obj[prop] */
-// const callOnProp = curry((field, repository, params) => {
-//   //
-//   const fn = repository[field];
-//   return fn(params);
-// });
-
-// /** @: callOnProp :: prop -> obj -> call obj[prop] */
-// const callOnProp = curry((field, repository, params) => {
-//   //
-//   const fn = repository[field];
-//   return fn(params);
-// });
+const { curry, compose, prop } = require('ramda');
 
 /** @: toParams :: fn -> args -> fn(args) */
 const toParams = curry((fn, args) => fn(args));
 
 /** @: callOnProp :: prop -> obj -> (a -> b) -> call obj[prop], params */
 const callOnProp = curry(compose(toParams, prop));
-
-// /** @: callOnProp :: prop -> obj -> call obj[prop] */
-// const callOnProp = curry(compose(always, toParams, prop));
 
 module.exports = callOnProp;
