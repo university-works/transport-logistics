079a0093bbe6411bb69882db6bf017b715fb839d
RV8V <rv.volovik88@gmail.com>
2022-04-15 00:40:21 +0300

    feat: add ui dependency for openapi, update services, .env.example

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -5,15 +5,20 @@ const bodyParser = require('body-parser');
 const { future } = cast;
 
 const express = future(require('express'));
-const { setUpRoutes } = require('./services/index');
+const { setUpRoutes, serveOpenapi } = require('./services/index');
+const { config } = require('../config/index');
 const rs = require('./routes');
 
-const { apFields, appUse } = require('../utils/express/index');
+const { apFields, appUse, appUseAsItIs } = require('../utils/express/index');
+const { path } = require('../utils/swagger/index');
 
 const apBodyParse = apFields(bodyParser);
 
+const filePath = config.get('FILE_PATH');
+
 const app = compose(
   appUse(setUpRoutes(rs.router))(rs.routes),
+  appUseAsItIs(serveOpenapi)(path(filePath)),
   apBodyParse('json')({}),
   apBodyParse('urlencoded')({ extended: false }),
   call,
